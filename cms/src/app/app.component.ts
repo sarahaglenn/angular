2f892f7b0a0dc6d266711c9779106b4a5ff24d1c
Sarah Glenn <sar00@byui.edu>
2026-01-22 12:32:32 +0100

    implement tab selection in header

--- cms/src/app/app.component.ts
+++ cms/src/app/app.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cms';
+  selectedFeature: string = 'documents';
+
+  switchView(selectedFeature: string) {
+    this.selectedFeature = selectedFeature;
+  }
 }
--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
@@ -32,7 +33,8 @@ import { MessageListComponent } from './message/message-list/message-list.compon
     MessageListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
