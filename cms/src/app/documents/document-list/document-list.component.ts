6c97342a4db8e22543021d7bd691b90278433744
Sarah Glenn <sar00@byui.edu>
2026-02-03 10:42:00 +0100

    Implement the document service

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'cms-document-list',
@@ -8,41 +9,14 @@ import { Document } from '../document.model';
   templateUrl: './document-list.component.html',
   styleUrl: './document-list.component.css',
 })
-export class DocumentListComponent {
+export class DocumentListComponent implements OnInit{
+  documents: Document[] = [];
   @Output() selectedDocumentEvent = new EventEmitter<Document>();
-  documents: Document[] = [
-    new Document(
-      1,
-      'WDD 430 - Week2 Assignment',
-      'An important document.',
-      'documents/document1',
-    ),
-    new Document(
-      2,
-      'WDD 430 - Week1 Assignment',
-      'An very important document.',
-      'documents/document2',
-    ),
-    new Document(
-      3,
-      'CSE 430 - Milestone 1',
-      'Another document.',
-      'documents/document3',
-    ),
-    new Document(
-      4,
-      'CSE 430 - Milestone 2',
-      'An essential document.',
-      'documents/document4',
-    ),
-    new Document(
-      5,
-      'WDD 430 - Week3 Assignment',
-      'A really cool document.',
-      'documents/document5',
-    ),
-  ];
+  constructor(private documentService: DocumentService) {}
 
+  ngOnInit(): void {
+    this.documents = this.documentService.getDocuments();
+  }
   onSelectedDocument(document: Document) {
     this.selectedDocumentEvent.emit(document);
   }
---
+++ cms/src/app/documents/document.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+import { Document } from './document.model';
+import { MOCKDOCUMENTS } from './mockdocuments';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DocumentService {
+  private documents: Document[];
+
+  constructor() {
+    this.documents = MOCKDOCUMENTS;
+  }
+
+  getDocuments(): Document[] {
+    return this.documents.slice();
+  }
+  getDocument(id:string): Document {
+    this.documents.forEach((document: Document) => {
+      if (document.id === id)
+      {
+        return document;
+      }
+    })
+    return null;
+  }
+}
