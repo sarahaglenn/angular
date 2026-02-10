72d91f899998ea16c31054a376aa310d104b1427
Sarah Glenn <sar00@byui.edu>
2026-02-10 16:17:05 +0100

    define child routes for document items

--- cms/src/app/documents/document-detail/document-detail.component.ts
+++ cms/src/app/documents/document-detail/document-detail.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'cms-document-detail',
@@ -8,6 +10,22 @@ import { Document } from '../document.model';
   templateUrl: './document-detail.component.html',
   styleUrl: './document-detail.component.css'
 })
-export class DocumentDetailComponent {
-  @Input() document: Document;
+export class DocumentDetailComponent implements OnInit{
+  document: Document;
+  id: string;
+
+  constructor(private documentService: DocumentService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params
+    .subscribe(
+      (params: Params) => {
+        this.id =params['id'];
+        this.document = this.documentService.getDocument(this.id);
+      }
+    )
+  }
 }
--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -16,7 +16,4 @@ export class DocumentListComponent implements OnInit{
   ngOnInit(): void {
     this.documents = this.documentService.getDocuments();
   }
-  onSelectedDocument(document: Document) {
-    this.documentService.documentSelectedEvent.emit(document);
-  }
 }
