cb8ff36eabd49ad736d0b4cf48c19e2f72e4daac
Sarah Glenn <sar00@byui.edu>
2026-02-10 11:35:11 +0100

    Create document edit component

--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { MessageEditComponent } from './messages/message-edit/message-edit.compo
 import { MessageListComponent } from './messages/message-list/message-list.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { AppRoutingModule } from './app-routing.module';
+import { DocumentEditComponent } from './documents/document-edit/document-edit.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -32,7 +33,8 @@ import { AppRoutingModule } from './app-routing.module';
     MessageItemComponent,
     MessageEditComponent,
     MessageListComponent,
-    DropdownDirective
+    DropdownDirective,
+    DocumentEditComponent
   ],
   imports: [
     BrowserModule,
