dedb4c5da4605d607e22e6c246635694847886bd
Sarah Glenn <sar00@byui.edu>
2026-02-10 11:28:57 +0100

    Specify where to display routed components

--- cms/src/app/app.component.ts
+++ cms/src/app/app.component.ts
@@ -7,10 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'cms';
-  selectedFeature: string = 'documents';
-
-  switchView(selectedFeature: string) {
-    this.selectedFeature = selectedFeature;
-  }
+  title = 'WeLearn CMS!';
 }
--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MessageItemComponent } from './messages/message-item/message-item.compo
 import { MessageEditComponent } from './messages/message-edit/message-edit.component';
 import { MessageListComponent } from './messages/message-list/message-list.component';
 import { DropdownDirective } from './shared/dropdown.directive';
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -35,7 +36,8 @@ import { DropdownDirective } from './shared/dropdown.directive';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
