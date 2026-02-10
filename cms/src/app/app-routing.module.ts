f481fbee4cf1d0f53a71397cc49e4c4d40aab968
Sarah Glenn <sar00@byui.edu>
2026-02-10 11:17:58 +0100

    Define routes

---
+++ cms/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { RouterModule, Routes } from "@angular/router";
+import { NgModule } from "@angular/core";
+
+import { DocumentsComponent } from "./documents/documents.component";
+import { MessageListComponent } from "./messages/message-list/message-list.component";
+import { ContactsComponent } from "./contacts/contacts.component";
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/documents', pathMatch: 'full' },
+  { path: 'documents', component: DocumentsComponent },
+  { path: 'messages', component: MessageListComponent },
+  { path: 'contacts', component: ContactsComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+
+}
