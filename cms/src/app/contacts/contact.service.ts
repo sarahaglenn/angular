ea6951de746512b2b817b10ad90303a5168de877
Sarah Glenn <sar00@byui.edu>
2026-02-16 22:03:07 +0100

    Add update and add contact functions

--- cms/src/app/contacts/contact.service.ts
+++ cms/src/app/contacts/contact.service.ts
@@ -11,9 +11,11 @@ export class ContactService {
   private contacts: Contact[] = [];
   contactSelectedEvent = new EventEmitter<Contact>();
   contactListChangedEvent = new Subject<Contact[]>();
+  maxContactId: number;
 
   constructor() {
     this.contacts = MOCKCONTACTS;
+    this.maxContactId = this.getMaxId();
    }
 
   getContacts(): Contact[] {
@@ -36,5 +38,34 @@ export class ContactService {
     }
     this.contacts.splice(pos, 1);
     this.contactListChangedEvent.next(this.contacts.slice());
+  }
+
+  addContact( newContact: Contact) {
+    if (!newContact) return;
+
+    this.maxContactId++;
+    newContact.id = String(this.maxContactId);
+    this.contacts.push(newContact);
+    this.contactListChangedEvent.next(this.contacts.slice());
+  }
+
+  updateContact( originalContact: Contact, newContact: Contact) {
+    if (!originalContact || !newContact) return;
+
+    const pos = this.contacts.indexOf(originalContact);
+    if (pos < 0 ) return;
+    newContact.id = originalContact.id;
+    this.contacts[pos] = newContact;
+    this.contactListChangedEvent.next(this.contacts.slice())
+  }
+
+  getMaxId(): number {
+    let maxId = 0;
+    this.contacts.forEach( (contact) => {
+      if (parseInt(contact.id) > maxId) {
+        maxId = parseInt(contact.id);
+      }
+    })
+    return maxId;
   }
 }
