9a20f0759f4530359cda1f2d7c544e19c9a8f38e
Sarah Glenn <sar00@byui.edu>
2026-02-24 21:59:55 +0100

    Implement add and edit documents

--- cms/src/app/documents/document-edit/document-edit.component.ts
+++ cms/src/app/documents/document-edit/document-edit.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { DocumentService } from '../document.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-edit',
   standalone: false,
   templateUrl: './document-edit.component.html',
-  styleUrl: './document-edit.component.css'
+  styleUrl: './document-edit.component.css',
 })
-export class DocumentEditComponent {
+export class DocumentEditComponent implements OnInit {
+  originalDocument: Document;
+  document: Document;
+  editMode: boolean = false;
+  id: string;
 
+  constructor(
+    private documentService: DocumentService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = params['id'];
+          if (!this.id) {
+            this.editMode = false;
+            return;
+          }
+          this.originalDocument = this.documentService.getDocument(this.id);
+          if (!this.originalDocument) {
+            return;
+          }
+          this.editMode = true;
+          this.document = JSON.parse(JSON.stringify(this.originalDocument));
+        });
+  }
+
+  onSubmit(form: NgForm) {
+    const newDocument = new Document(
+      this.id,
+      form.value['name'],
+      form.value['description'],
+      form.value['url']
+    );
+    if (this.editMode) {
+      this.documentService.updateDocument(this.originalDocument, newDocument);
+    } else {
+      this.documentService.addDocument(newDocument);
+    }
+    this.onCancel();
+  }
+  onCancel() {
+    this.router.navigate(['../'], { relativeTo: this.route });
+  }
 }
