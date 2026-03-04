23c63f4d746cc6b72fae57fe3c2231d3929dc62c
Sarah Glenn <sar00@byui.edu>
2026-03-04 07:37:04 +0100

    Modify MessageService to preform HTTP get/put requests

--- cms/src/app/messages/message-list/message-list.component.ts
+++ cms/src/app/messages/message-list/message-list.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { Message } from '../message.model';
 import { MessageService } from '../message.service';
+import { Subscription } from 'rxjs';
+import { ContactService } from '../../contacts/contact.service';
 
 @Component({
   selector: 'cms-message-list',
@@ -9,18 +11,25 @@ import { MessageService } from '../message.service';
   templateUrl: './message-list.component.html',
   styleUrl: './message-list.component.css',
 })
-export class MessageListComponent implements OnInit{
+export class MessageListComponent implements OnInit, OnDestroy {
   messages: Message[] = [];
+  subscription: Subscription;
 
-  constructor(private messageService: MessageService) {}
+  constructor(private messageService: MessageService,
+              private contactService: ContactService
+  ) {}
 
   ngOnInit(): void {
-    this.messages = this.messageService.getMessages();
-    this.messageService.messageChangedEvent
+    this.contactService.getContacts();
+    this.subscription = this.messageService.messageChangedEvent
     .subscribe(
-      (messages: Message[]) => {
-        this.messages = messages;
+      (messagesList: Message[]) => {
+        this.messages = messagesList;
       }
-    )
+    );
+    this.messageService.getMessages();
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
--- cms/src/app/messages/message.service.ts
+++ cms/src/app/messages/message.service.ts
@@ -1,30 +1,59 @@
 import { EventEmitter, Injectable } from '@angular/core';
 
 import { Message } from './message.model';
-import { MOCKMESSAGES } from './mockmessages';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MessageService {
-  private messages: Message[]= [];
+  private messages: Message[] = [];
   messageChangedEvent = new EventEmitter<Message[]>();
+  maxMessageId: number;
 
-  constructor() {
-    this.messages = MOCKMESSAGES;
-   }
+  constructor(private http: HttpClient) { }
 
-   getMessages(): Message[] {
-    return this.messages.slice();
-   }
+  getMessages() {
+    this.http
+    .get<Message[]>('https://contact-manag-sys-default-rtdb.firebaseio.com/messages.json')
+    .subscribe(
+      (messages: Message[]) => {
+        this.messages = messages || [];
+        this.maxMessageId = this.getMaxId();
+        this.messageChangedEvent.next(this.messages.slice());
+      },
+      (error: any) => {
+        console.log(error.message)
+      }
+    )
+  }
 
-   getMessage(id: string): Message {
-    return this.messages.find(
-      (message: Message) => message.id === id) || null;
-   }
+  storeMessages() {
+    this.http
+    .put('https://contact-manag-sys-default-rtdb.firebaseio.com/messages.json',
+      this.messages
+    )
+    .subscribe(() => {
+      this.messageChangedEvent.next(this.messages.slice());
+    })
+  }
 
-   addMessage(message: Message) {
+  getMessage(id: string): Message {
+    return this.messages.find((message: Message) => message.id === id) || null;
+  }
+
+  addMessage(message: Message) {
     this.messages.push(message);
-    this.messageChangedEvent.emit(this.messages.slice());
-   }
+    this.storeMessages();
+  }
+
+  getMaxId(): number {
+    let maxId = 0;
+    this.messages.forEach((message) => {
+      if (parseInt(message.id) > maxId) {
+        maxId = parseInt(message.id);
+      }
+    });
+    return maxId;
+  }
 }
