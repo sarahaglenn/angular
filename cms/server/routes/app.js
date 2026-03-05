6a4e9c62bc1b78cbc188c898822ecc9b414e0c2c
Sarah Glenn <sar00@byui.edu>
2026-03-05 07:20:08 +0100

    Create Node.js/Express project

---
+++ cms/server/routes/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../../dist/cms/browser/index.html'));
+});
+
+module.exports = router;
